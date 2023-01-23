c06eb9ed3b8c7655adc9c072dcf7d2e89243e494
Avi <avi.aminov@authomize.com>
2023-01-23 10:46:50 +0200

    remove redundant files

--- build/lib/datumaro/cli/commands/remove.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2019-2021 Intel Corporation
-#
-# SPDX-License-Identifier: MIT
-
-from ..contexts.source import build_remove_parser as build_parser
-
-__all__ = [
-    'build_parser',
-]
--- build/lib/datumaro/cli/contexts/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# Copyright (C) 2019-2021 Intel Corporation
-#
-# SPDX-License-Identifier: MIT
-
-from . import model, project, source
